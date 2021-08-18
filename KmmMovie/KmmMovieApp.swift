58092f21876a4aa8bdb4915a3897e614c428ec61
Venky <maganahalli@gmail.com>
2021-08-18 14:01:22 -0400

    Initial Commit

---
+++ KmmMovie/KmmMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  KmmMovieApp.swift
+//  KmmMovie
+//
+//  Created by Venkatesh Maganahalli on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct KmmMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
